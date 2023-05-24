4d78b8a50c91ac264317d7ea1f95cbb7afd603a5
th3worst4 <caiocouto25@hotmail.com>
2023-05-24 18:56:09 -0300

    modified:   minmax.py

--- minmax.py
+++ minmax.py
@@ -1,35 +1,40 @@
 initialPointMatrix = [[3, 2, 3], [2, 1, 2], [3, 2, 3]]
 
-def sumDiagonal(initialPointMatrix, s = False):
+def sumDiagonal(pointMatrix, s = False):
     sum = 0
     for i in range(3):
         if not s:
-            sum += initialPointMatrix[i][i]
+            sum += pointMatrix[i][i]
         else:
-            sum += initialPointMatrix[2-i][i]
+            sum += pointMatrix[2-i][i]
     return sum
 
-def pointSum(line, column):
-    initialPointMatrix[line-1][column-1] = 0
+def changingValues(line, column, pointMatrix, firstPlay):
+    if firstPlay:
+        pointMatrix[line-1][column-1] = 0
+    else:
+        pointMatrix[line-1][column-1] = 10
+
+def pointSum(line, column, pointMatrix):
     vectorSums = [0, 0]
     coord = (line-1, column-1)
 
     for i in range(3):
-        vectorSums[0] += initialPointMatrix[line-1][i]
-        vectorSums[1] += initialPointMatrix[i][column-1]
+        vectorSums[0] += pointMatrix[line-1][i]
+        vectorSums[1] += pointMatrix[i][column-1]
 
     if coord not in [(1, 0), (0, 1), (2, 1), (1, 2)]:
         if coord == (1, 1):
-            vectorSums.append(sumDiagonal(initialPointMatrix))
-            vectorSums.append(sumDiagonal(initialPointMatrix, True))
+            vectorSums.append(sumDiagonal(pointMatrix))
+            vectorSums.append(sumDiagonal(pointMatrix, True))
             print("case 1")
         elif coord not in [(0, 2), (2, 0)]:
-            vectorSums.append(sumDiagonal(initialPointMatrix))
+            vectorSums.append(sumDiagonal(pointMatrix))
             print("case 2")
         else:
-            vectorSums.append(sumDiagonal(initialPointMatrix, True))
+            vectorSums.append(sumDiagonal(pointMatrix, True))
             print("case 3")
     
     return vectorSums
 
-print(pointSum(1, 3))
+print(pointSum(1, 3, initialPointMatrix))
