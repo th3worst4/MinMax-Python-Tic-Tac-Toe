0229b0d4f29295c1bc8ed826e0cfcb1f08ddb401
th3worst4 <caiocouto25@hotmail.com>
2023-05-25 20:10:30 -0300

    new file:   possiblegames.py

---
+++ possiblegames.py
@@ -0,0 +1,69 @@
+def play():
+    possibleGames = []
+    symb = 1
+    round = 0
+    for i in range(9):
+        matrix = [0,0,0,0,0,0,0,0,0]
+        matrix[i] = symb
+        possibleGames.append(matrix)
+    
+    round += 1
+    nextRound(symb, possibleGames, round)
+
+def nextRound(symb, possibleGames, round, pnt1 = 0, pnt2 = 0):
+    if round == 9:
+        print(pnt1)
+        print(pnt2)
+        print(len(possibleGames))
+        return
+    else:
+        symb *= -1
+        newPossible = list()
+        for game in possibleGames:
+            for i in range(9):
+                aux = list(game)
+                if game[i] == 0:
+                    aux[i] = symb
+                    if checkVictory(organazingGame(aux), symb):
+                        if symb == 1:
+                            pnt1+=1
+                        else:
+                            pnt2-=1
+                        continue
+                    newPossible.append(aux)
+                else:
+                    continue
+        round+=1
+        nextRound(symb, newPossible, round, pnt1, pnt2)
+
+def organazingGame(game):
+    matrix = [[0,0,0],[0,0,0],[0,0,0]]
+    index = 0
+    for i in range(3):
+        for j in range(3):
+            matrix[i][j] = game[index]
+            index+=1
+    return matrix
+
+def checkVictory(matrix, symb):
+    victory = False
+    
+    for i in range(3):
+        sum1 = matrix[i][0] + matrix[i][1] + matrix[i][2]
+        sum2 = matrix[0][i] + matrix[1][i] + matrix[2][i]
+        if sum1 == symb*3 or sum2 == symb*3:
+            victory = True
+        else:
+            continue
+
+    sum3 = 0
+    sum4 = 0  
+    for j in range(3):
+        sum3 += matrix[j][j]
+        sum4 += matrix[2-j][j]
+    if sum3 == symb*3 or sum4 == symb*3:
+        victory = True
+
+    return victory
+
+play()
